f2757b3491c2600af89f4abebb36922607fc92fd
Ruchi Ray <ruchiray18@gmail.com>
2021-12-19 02:35:14 +0530

    changed api

--- functions/fetch-cryptoInfo/fetch-cryptoInfo.js
+++ functions/fetch-cryptoInfo/fetch-cryptoInfo.js
@@ -1,22 +1,21 @@
 // Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
 const handler = async (event) => {
   const endPoint = event.queryStringParameters.type;
-  const limit = event.queryStringParameters.limit||100;
-  let offset = event.queryStringParameters.offset||0;
- 
- console.log('printing query parameters',event.queryStringParameters);
+  const limit = event.queryStringParameters.limit || 100;
+  let offset = event.queryStringParameters.offset || 0;
+
+  console.log("printing query parameters", event.queryStringParameters);
   var axios = require("axios").default;
   var options = {
     method: "GET",
-    url: `https://coinranking1.p.rapidapi.com/${endPoint}?limit=${limit}&&offset=${offset}`,
+    url: `https://api.coinranking.com/v2/${endPoint}?limit=${limit}&&offset=${offset}`,
     headers: {
-      "x-rapidapi-key": process.env.CRYPTO_API_KEY,
-      "x-rapidapi-host": "coinranking1.p.rapidapi.com",
+      "x-access-token": process.env.CRYPTO_API_KEY,
     },
   };
   try {
     const res = await axios(options);
-    const {data} = res;
+    const { data } = res;
     console.log("print res", data);
     return {
       statusCode: 200,
